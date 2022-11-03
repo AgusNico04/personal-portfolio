31b06e382d827b7c17dbdcacdf2773836d27a35b
AgusDev <aguspaterlinimfqa@gmail.com>
2022-11-02 22:35:48 -0300

    Updates

---
+++ precache-manifest.1a6d4341baf53cc6e3060bfec4c04d39.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a13229bea20117dca3f",
+    "url": "/personal-portfolio/static/css/main.93fafbfc.chunk.css"
+  },
+  {
+    "revision": "6a13229bea20117dca3f",
+    "url": "/personal-portfolio/static/js/main.6a13229b.chunk.js"
+  },
+  {
+    "revision": "8addee3f2ae467e873ea",
+    "url": "/personal-portfolio/static/js/1.8addee3f.chunk.js"
+  },
+  {
+    "revision": "fc41262401350ab4c027",
+    "url": "/personal-portfolio/static/js/runtime~main.fc412624.js"
+  },
+  {
+    "revision": "38573dd73dd96572544533c1bf87b107",
+    "url": "/personal-portfolio/static/media/contact.38573dd7.png"
+  },
+  {
+    "revision": "03804760cb2b8ab1a65d889ccd8bf90b",
+    "url": "/personal-portfolio/static/media/projects.03804760.png"
+  },
+  {
+    "revision": "63cf9c6f8153aef1e884b7ae2815e06d",
+    "url": "/personal-portfolio/static/media/js.63cf9c6f.png"
+  },
+  {
+    "revision": "5bb68a291b93359dcc0e7fead45bbbb2",
+    "url": "/personal-portfolio/static/media/html5.5bb68a29.png"
+  },
+  {
+    "revision": "b5a77db30f1e49356f578c0d3aaf4621",
+    "url": "/personal-portfolio/static/media/css3.b5a77db3.png"
+  },
+  {
+    "revision": "fe21ed975ad3db4642308c558cbaa4da",
+    "url": "/personal-portfolio/static/media/nodejs.fe21ed97.png"
+  },
+  {
+    "revision": "3dcbcc916fa172fa5c9a597da848f513",
+    "url": "/personal-portfolio/static/media/nestjs.3dcbcc91.png"
+  },
+  {
+    "revision": "191dc5e93bbdb90d5ce0371b2a05295b",
+    "url": "/personal-portfolio/static/media/expressjs.191dc5e9.png"
+  },
+  {
+    "revision": "ad04e022c7a2be6bddba49886c59f9b2",
+    "url": "/personal-portfolio/static/media/angular.ad04e022.png"
+  },
+  {
+    "revision": "deee9796a9983b97891d175459ecfbc9",
+    "url": "/personal-portfolio/static/media/react.deee9796.png"
+  },
+  {
+    "revision": "fb224fe58513b1ab6b8d2a0921df5bb2",
+    "url": "/personal-portfolio/static/media/java.fb224fe5.png"
+  },
+  {
+    "revision": "656d2b189bb33bd9f778d7dc2f1d9b47",
+    "url": "/personal-portfolio/static/media/python.656d2b18.png"
+  },
+  {
+    "revision": "675908d255dd3814c650f0147e43c191",
+    "url": "/personal-portfolio/static/media/csharp.675908d2.png"
+  },
+  {
+    "revision": "6d50954cf73a00698aa5718dfc70cef5",
+    "url": "/personal-portfolio/static/media/github.6d50954c.png"
+  },
+  {
+    "revision": "0d89de82e025a517b00ae35e265bc16b",
+    "url": "/personal-portfolio/index.html"
+  }
+];
