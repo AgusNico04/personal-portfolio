5c2adb9d0fe0289d240621898b2f4c00f4bfe85c
AgusDev <aguspaterlinimfqa@gmail.com>
2022-11-05 21:08:12 -0300

    Added Projects

--- src/components/projectsSection/ProjectsSection.jsx
+++ src/components/projectsSection/ProjectsSection.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import chatProject from '../../assets/projectsSection/real_time_chat.png';
+import loginProject from '../../assets/projectsSection/login_node.png';
+import ticTacToeProject from '../../assets/projectsSection/tic_tac_toe.png';
+import crudProject from '../../assets/projectsSection/crud_project.png';
 import './ProjectsSection.css';
 
 export default function ProjectsSection() {
@@ -9,11 +13,11 @@ export default function ProjectsSection() {
             </div>
             <div id='projects-list'>
                 <ul>
-                    <li>Project 1</li>
-                    <li>Project 2</li>
-                    <li>Project 3</li>
-                    <li>Project 4</li>
-                    <li>Project 5</li>
+                    <li><p>Real Time Chat</p><img src={chatProject} alt='Real time chat project' width={90} height={90}/><a href='https://real-time-chat-04.herokuapp.com' target='_blank' rel='noreferrer noopener'>Visit App</a></li>
+                    <li><p>Login with NodeJS</p><img src={loginProject} alt='Login with nodejs project' width={90} height={90}/><a href='https://login-node-production.up.railway.app/' target='_blank' rel='noreferrer noopener'>Visit App</a></li>
+                    <li><p>CRUD with NodeJS</p><img src={crudProject} alt='CRUD project' width={90} height={90}/><a href='https://crud-production-9a93.up.railway.app/' target='_blank' rel='noreferrer noopener'>Visit App</a></li>
+                    <li><p>Tic Tac Toe</p><img src={ticTacToeProject} alt='Tic Tac Toe project' width={90} height={90}/><a href='https://agusnico04.github.io/Tic-Tac-Toe' target='_blank' rel='noreferrer noopener'>Visit App</a></li>
+                    <li><p>Angular with C# (In progress)</p><img src="" alt='? Project' width={90} height={90}/></li>
                 </ul>
             </div>
         </section>
