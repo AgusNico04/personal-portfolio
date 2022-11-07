3d5914991140bf7786fc829e97f014947df9a64d
AgusDev <aguspaterlinimfqa@gmail.com>
2022-11-06 22:37:51 -0300

    Improved Design

---
+++ src/components/aboutSection/AboutSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Presentation from '../presentation/Presentation';
+import './AboutSection.css';
+
+export default function AboutSection() {
+    return (
+        <Presentation />
+    );
+}
